a592a5ea2e6f74f8f63ec14b625a6279da316123
Ariel Calix <acalixv@unicah.edu>
2024-06-11 22:22:25 -0700

    update axios config

--- src/axiosConfig/axiosConfig.tsx
+++ src/axiosConfig/axiosConfig.tsx
@@ -1,11 +1,12 @@
 import { AxiosRequestConfig } from "axios";
 
 const baseUrl = {
-    dev: 'http://51.161.13.38:3000/api'
+    dev: 'http://51.161.13.38:3000/api',
+    prod: 'https://apitecnofest.onrender.com'
 }
 
 const config: AxiosRequestConfig = {
-    baseURL: baseUrl.dev,
+    baseURL: baseUrl.prod,
     headers: {
         'content-type': 'application/json',
         'access-control-allow-origin': '*',
